be2175a92a7f3489e8f1c846b3eab20e41a6e444
Pouya Kary <k@karyfoundation.org>
2017-09-25 16:32:49 +0330

    added functions and constants

--- source/core/octobass/adapter.ts
+++ source/core/octobass/adapter.ts
@@ -62,6 +62,7 @@ namespace Workout.OctobassAdapter {
                     .join('\n')
 
             const funcString = (`(( ) => {
+                const { PI, E, abs, acos, acosh, asin, asinh, atan, atan2, atanh, cbrt, ceil, clz32, cos, cosh, exp, expm1, floor, fround, hypot, imul, log, log10, log1p, log2, max, min, pow, random, round } = Math;
                 ${ functionDataAsConstants }
                 return ${ input.formula };
             })( )`)
